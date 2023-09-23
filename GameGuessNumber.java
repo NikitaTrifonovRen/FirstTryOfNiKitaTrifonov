b8ef8674958f67613f76dc6769188c14b274baaf
NikitaTrifonovRen <145368284+NikitaTrifonovRen@users.noreply.github.com>
2023-09-23 18:02:38 +0500

    Update GameGuessNumber.java

--- GameGuessNumber.java
+++ GameGuessNumber.java
@@ -1,27 +1,22 @@
 import java.util.Scanner;
 public class GameGuessNumber {
     Scanner scanner;
-
     public static void main(String[] args) {
         new GameGuessNumber().gameLogic();
     }
-
     GameGuessNumber() {
         scanner = new Scanner(System.in);
     }
-
     void gameLogic() {
         int y = (int) (Math.random() * 100);
         System.out.println("Я загадал целое число от 1 до 100, угадывай, брат");
-        int x;
-        int tryCount = 1;
-        x = scanner.nextInt();
-        if (x < y) {
-            System.out.println("Я сам в шоке, но, загаданное число больше, брат");
-        } else if (x > y) {
-            System.out.println("Не ожидал от тебя такого. Загаданное число меньше, брат");
-        }
+        int x = -1;
+        int tryCount = 0;
         while (x != y) {
+            while (!scanner.hasNextInt()) {
+                scanner.next();
+                System.out.println("Это не число, давай по новой, братишка");
+            }
             x = scanner.nextInt();
             tryCount += 1;
             if (x < y) {
@@ -34,5 +29,4 @@ public class GameGuessNumber {
         System.out.println("Ты справился с " + tryCount + " попытки");
     }
 }
-
 
