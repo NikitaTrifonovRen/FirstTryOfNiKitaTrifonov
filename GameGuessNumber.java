539d6892482ce7933c3c85e9d65d968758f0b382
NikitaTrifonovRen <145368284+NikitaTrifonovRen@users.noreply.github.com>
2023-09-23 13:34:24 +0500

    Add files via upload

---
+++ GameGuessNumber.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class GameGuessNumber {
+    Scanner scanner;
+
+    public static void main(String[] args) {
+        new GameGuessNumber().gameLogic();
+    }
+
+    GameGuessNumber() {
+        scanner = new Scanner(System.in);
+    }
+
+    void gameLogic() {
+        int y = (int) (Math.random() * 100);
+        System.out.println(y);
+        System.out.println("Я загадал целое число от 1 до 100, угадывай, брат");
+        int x;
+        int tryCount = 1;
+        x = scanner.nextInt();
+        if (x < y) {
+            System.out.println("Я сам в шоке, но, загаданное число больше, брат");
+        } else if (x > y) {
+            System.out.println("Не ожидал от тебя такого. Загаданное число меньше, брат");
+        }
+        while (x != y) {
+            x = scanner.nextInt();
+            tryCount += 1;
+            if (x < y) {
+                System.out.println("Я сам в шоке, но, загаданное число больше, брат");
+            } else if (x > y) {
+                System.out.println("Не ожидал от тебя такого. Загаданное число меньше, брат");
+            }
+        }
+        System.out.println("Я загадал " + y);
+        System.out.println("Ты справился с " + tryCount + " попытки");
+    }
+}
+
+
